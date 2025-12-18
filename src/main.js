6ffafb8f3232c04d42b414e6d0433fbef8bb1e32
aboub95 <aboub8240@gmail.com>
2025-12-18 12:16:08 +0100

    remove vite.svg references

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import './style.css'
 import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
+
 import { setupCounter } from './counter.js'
 
 // Remplacez par votre URL SheetDB API
